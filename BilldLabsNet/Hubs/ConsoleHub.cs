78d61b57678a0e96698cf2ec18f6df012853fdc4
BillStrait <bill.strait@gmail.com>
2021-08-10 15:56:51 -0500

    initial commit. Signal R stuff is working.

---
+++ BilldLabsNet/Hubs/ConsoleHub.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNet.SignalR;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace BilldLabsNet.Hubs
+{
+    public class ConsoleHub : Hub
+    {
+        public void Send(string command)
+        {
+            var response = "Invalid text.";
+            if(string.Equals(command, "white rabbit", StringComparison.OrdinalIgnoreCase))
+            {
+                response = "How deep does the rabbit hole go?";
+            }
+            Clients.Caller.updateResponse(response);
+        }
+    }
+}
